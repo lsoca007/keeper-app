dee983b5d88ff48681d4f1aef3ce6c602167bdc8
lsoca007 <77649258+lsoca007@users.noreply.github.com>
2022-07-14 16:12:24 -0400

    Update App
     - add multiples notes

--- src/App.js
+++ src/App.js
@@ -4,13 +4,20 @@ import './components/Header/Header';
 import Header from './components/Header/Header';
 import Footer from './components/Footer';
 import Note from './components/Note/Note';
+import notes from './components/Note/notes';
+
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      {notes.map(  noteItem => 
+                  <Note key = {noteItem.key}
+                        title={noteItem.title}
+                        content={noteItem.content}
+                  />
+                )}
       <Footer/>
-      <Note/>
     </div>
   );
 }
