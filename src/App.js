c2aa2d71b8158595f0d2895033cbb11bb6cbcedd
lsoca007 <77649258+lsoca007@users.noreply.github.com>
2022-07-18 15:14:52 -0400

    chore: add login and register form

--- src/App.js
+++ src/App.js
@@ -5,18 +5,23 @@ import Header from './components/Header/Header';
 import Footer from './components/Footer';
 import Note from './components/Note/Note';
 import notes from './components/Note/notes';
+import Login from './components/Login/login';
 
+
+var isLoggedIn = false;
+var userIsRegisterd = true;
 
 function App() {
   return (
-    <div className="App">
+    <div className="App"> 
       <Header/>
-      {notes.map(  noteItem => 
+      {isLoggedIn === true ? 
+      notes.map(  noteItem => 
                   <Note key = {noteItem.key}
                         title={noteItem.title}
                         content={noteItem.content}
                   />
-                )}
+                ) : <Login isRegistered = {userIsRegisterd}/>}     
       <Footer/>
     </div>
   );
